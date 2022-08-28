dc2b57ab40ca7853617fc143c46f17088cf36b79
M. Raka Maulana A <rmalan09@outlook.com>
2022-08-28 21:33:55 +0700

    validation length title

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -7,6 +7,7 @@ class NoteInput extends React.Component {
     this.state = {
       title: '',
       body: '',
+      maxChar: 50,
     }
 
     this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this);
@@ -18,7 +19,7 @@ class NoteInput extends React.Component {
   onTitleChangeEventHandler(event) {
     this.setState(() => {
       return {
-        title: event.target.value,
+        title: event.target.value.slice(0, this.state.maxChar),
       }
     });
   }
@@ -48,7 +49,7 @@ class NoteInput extends React.Component {
       <div className="note-input">
         <h2>Buat catatan</h2>
         <form onSubmit={this.onSubmitEventHandler}>
-          <p className="note-input__title__char-limit">Sisa karakter: 50</p>
+          <p className="note-input__title__char-limit">Sisa karakter: {this.state.maxChar - this.state.title.length}</p>
           <input className="note-input__title" type="text" placeholder="Ini adalah judul ..." value={this.state.title} onChange={this.onTitleChangeEventHandler} required />
           <textarea className="note-input__body" type="text" placeholder="Tuliskan catatanmu di sini ..." value={this.state.body} onChange={this.onBodyChangeEventHandler} required></textarea>
           <button type="submit">Buat</button>
